d281421c7e798b7c910978590a8125146cdf744d
vagrant <vagrant@homestead>
2019-03-04 09:22:19 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,13 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('/signup', 'UsersController@create')->name('signup');
 /*用户相关*/
 Route::resource('users','UsersController');
-
 /*登陆相关*/
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+/*密码重置相关*/
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
